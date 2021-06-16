c50b50e041e4ec3137500fa699584f55bda6a754
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-16 18:57:19 -0400

    Fixed text editor spacing

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -96,8 +96,7 @@
 
 <div class="form-group">
     <label for="post_content">Post Content</label>
-    <textarea type="text" class="form-control" name="post_content" id="" cols="30" rows="10">
-    </textarea>
+    <textarea type="text" class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
 </div>
 <div class="form-group">
           <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
