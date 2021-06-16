684c31e822a485c453eb1e22aa230a6f3cb1905d
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-16 18:28:36 -0400

    Added draft status toggle on post

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -118,8 +118,30 @@
 </div>
 
 <div class="form-group">
-    <label for="post_status">Post Status</label>
-    <input value="<?php echo $post_status?>" type="text" class="form-control" name="post_status">
+<label for="post_status">Post Status</label>
+<select name = "post_status" id="">
+<option value="<?php echo $post_status?>"><?php echo $post_status?></option>
+
+
+<?php 
+ if($post_status == 'published'){
+    echo '<option value="draft">Draft</option>';
+ }else{
+     echo '<option value="published">Published</option>';
+ }
+
+
+?>
+
+
+
+</select>
+
+
+
+
+    
+<!--<input value="<?php echo $post_status?>" type="text" class="form-control" name="post_status"> -->
 </div>
 
 
