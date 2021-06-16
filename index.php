1d6a144ad6aa51070203b8829a0faad66c4e7439
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-16 13:18:12 -0400

    changed page name

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
             <h1 class="page-header">
-                    Page Heading
+                    Page
                     <small>Secondary Text</small>
                     </h1>
 
