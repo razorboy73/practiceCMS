13029a60a6cf4ad10519e9809157587138c36ebc
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-19 19:34:52 -0400

    online user count added

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,34 @@
 <?php include "includes/admin-header.php"; ?>
 
     <div id="wrapper">
+<?php
 
+$session = session_id();
+$time = time();
+$time_out_in_seconds = 60;
+$time_out = $time - $time_out_in_seconds;
+
+$query = "SELECT * FROM users_online WHERE session = '$session'";
+$send_query = mysqli_query($connection, $query);
+$count = mysqli_num_rows($send_query);
+
+if($count == NULL){
+    mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session','$time')");
+
+}else{
+    mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+}
+
+$users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
+
+$count_user = mysqli_num_rows($users_online_query);
+if(!$count_user ){
+    die('Query Failed: ' . mysqli_error($connection));
+}
+
+
+
+?>
 
 
         <!-- Navigation -->
@@ -25,6 +52,7 @@
                         <h1 class="page-header">
                            Administrative User Interface
                             <small> <?php echo $_SESSION['username']; ?></small>
+                            <?php echo $count_user; ?>
                         </h1>
                         
                     </div>
