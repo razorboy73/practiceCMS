748dfe59cc3aeaba26ede135903febcd829a9901
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-19 05:44:44 -0400

    added view count functionality

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -129,6 +129,7 @@
                     
                     <th>Edit</th>
                      <th>Delete</th>
+                     <th>Views</th>
                                     
                                 </tr>
                             </thead>
@@ -148,6 +149,7 @@
                                     $post_tags = $row['post_tags'];
                                     $post_comment_count = $row['post_comment_count'];
                                     $post_date = $row['post_date'];
+                                    $post_views_count = $row['post_views_count'];
 
 
                                     echo "<tr>";
@@ -176,7 +178,7 @@
                                     echo "<td>{$post_date}</td>";
                                     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                                     echo "<td><a onClick = \"javascript:return confirm('Are you sure about deleting this?'); \"  href='posts.php?delete={$post_id}'>Delete</a></td>";
-            
+                                    echo "<td>{$post_views_count}</td>";
                                     echo "</tr>";
                               }
 
--- post.php
+++ post.php
@@ -18,9 +18,16 @@
 
 
             <?php 
-                if(isset($_GET['p_id']))
+                if(isset($_GET['p_id'])){
                     $the_post_id = $_GET["p_id"];
-            
+
+                    $view_query = "UPDATE posts SET post_views_count = post_views_count +1 WHERE post_id = $the_post_id";
+                    $send_query = mysqli_query($connection, $view_query);
+
+                    if(!$send_query){
+                        die("query failed");
+                    }
+                    
             ?>
                 <h1 class="page-header">
                     Page Heading
@@ -59,7 +66,16 @@
 
                 <hr>
 
-                <?php }  ?>
+                <?php }
+
+
+                }else{
+            
+            header("Location: index.php");
+            
+            
+            
+            } ?>
 
 
                 <!-- Blog Comments -->
