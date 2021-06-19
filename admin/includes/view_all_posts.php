39d27c913da24c297dead074422fbe3fa20390b1
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-19 11:38:55 -0400

    fixed view reset and escaped query

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -129,7 +129,8 @@
                     
                     <th>Edit</th>
                      <th>Delete</th>
-                     <th>Views</th>
+                     <th>Views (click to reset views)</th>
+                
                                     
                                 </tr>
                             </thead>
@@ -178,7 +179,7 @@
                                     echo "<td>{$post_date}</td>";
                                     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                                     echo "<td><a onClick = \"javascript:return confirm('Are you sure about deleting this?'); \"  href='posts.php?delete={$post_id}'>Delete</a></td>";
-                                    echo "<td>{$post_views_count}</td>";
+                                    echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
                                     echo "</tr>";
                               }
 
@@ -200,4 +201,17 @@
                             $query = "DELETE from posts WHERE post_id = {$the_post_id} ";
                             $delete_query = mysqli_query($connection, $query);
                             header("Location: posts.php");
-                        }
+                          }
+                      
+                        ?>
+                         <?php
+                          if(isset($_GET['reset'])){  
+                            $the_post_id = $_GET['reset'];
+                    
+                            $query = "UPDATE posts SET post_views_count = 0 WHERE post_id =" . mysqli_real_escape_string($connection, $_GET['reset']) ." ";
+                            $delete_query = mysqli_query($connection, $query);
+                            header("Location: posts.php");
+                          
+                          }
+                      
+                        ?>
