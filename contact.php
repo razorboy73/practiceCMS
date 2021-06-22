61c520897af248d82a9e169f7e3ab4bfb2f455f2
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-22 12:28:40 -0400

    updated contact form

--- contact.php
+++ contact.php
@@ -1,49 +1,14 @@
 <?php  include "includes/db.php"; ?>
  <?php  include "includes/header.php"; ?>
  <?php
-    if(isset($_POST['submit'])){
+    if(isset($_POST['contact'])){
 
        
-        $username = $_POST['username'];
-        $user_email = $_POST['email'];
-        $user_password =$_POST['password'];
+        $to= $_POST['email'];
+        $subject = $_POST['subject'];
+        $body =$_POST['body'];
 
-        if(!empty($username) && !empty($user_email) && !empty($user_password)){
-
-            $username = mysqli_real_escape_string($connection,$username);
-            $user_email= mysqli_real_escape_string($connection,$user_email);
-            $user_password = mysqli_real_escape_string($connection,$user_password );
-
-
-            $user_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 12));
-
-    
-
-            $query = "INSERT into users(user_role,username,user_email,user_password) ";
-            $query .= "VALUE('subscriber','{$username}', '{$user_email}','{$user_password}') ";
-
-
-            $register_user_query = mysqli_query($connection, $query);
-
-            if (!$register_user_query){
-                die("Query failed ". mysqli_error($connection) . " " .mysqli_errno($connection));
-
-            }
-               
-            $message = "Your Registration has been submitted";
-           
-            
-
-        }else{
-             $message= 'Registration Fields Cannot Be Blank';
-
-            }
-        
-
-    }else{
-        $message = "";
     }
- 
     
  ?>
  
@@ -64,8 +29,8 @@
                 <div class="form-wrap">
                 <h1>Contact Us</h1>
             
-                    <form role="form" action="contact.php" method="post" id="login-form" autocomplete="off">
-                        <h6 class = "text center"><?php echo $message ?></h6>
+                    <form role="form" action="" method="post" id="login-form" autocomplete="off">
+                    
                       
                          <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
@@ -73,13 +38,13 @@
                         </div>
                         <div class="form-group">
                             <label for="subject" class="sr-only">Subject</label>
-                            <input type="email" name="subject" id="subject" class="form-control" placeholder="Enter Your Subject">
+                            <input type="text" name="subject" id="subject" class="form-control" placeholder="Enter Your Subject">
                         </div>
                          <div class="form-group">
-                            <textarea class="form-control" name="body" id="body" ></textarea>
+                            <textarea class="form-control" name="body" id="body"></textarea>
                         </div>
                 
-                        <input type="submit" name='contact" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Contact Us">
+                        <input type="submit" name="contact" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Contact Us">
                     </form>
                  
                 </div>
