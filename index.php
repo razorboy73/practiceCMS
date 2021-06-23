6c703a33c99a866c4f2770677d9e579c02e9567b
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-23 11:25:43 -0400

    added draft viewability for administrators

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include "includes/header.php"?>
 <?php include "includes/db.php" ?>
+<?php session_start(); ?>
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -35,55 +36,92 @@
                     $page_1 = ($page *$per_page)-$per_page;
                 }
 
-                $select_post_query_count = "SELECT * FROM posts WHERE post_status = 'published'";
-                $find_count = mysqli_query($connection, $select_post_query_count);
-                $count = mysqli_num_rows($find_count);
+                if(isset($_SESSION['user_role']) && ($_SESSION['user_role'] == 'admin')){
+                    $select_post_query_count= "SELECT * FROM posts";
+                    $find_count = mysqli_query($connection, $select_post_query_count);
+                    $count = mysqli_num_rows($find_count);
+                    $count = ceil($count/$per_page);
 
-                $count = ceil($count/$per_page);
-
-                if($count<1){
-                   
-                        echo "<h2> Nothing To See Here Folks</h2>";
                     
-
-                }else{
-                
-                   
-                
-                    $query = "SELECT * FROM posts  WHERE post_status = 'published' LIMIT $page_1, $per_page";
-                    $select_all_posts_query = mysqli_query($connection, $query);
-
-                    while($row = mysqli_fetch_assoc($select_all_posts_query)){
-                            $post_id= $row['post_id'];
-                            $post_title = $row['post_title'];
-                            $post_author = $row['post_author'];
-                            $post_date = $row['post_date'];
-                            $post_image = $row['post_image'];
-                            $post_content = substr($row['post_content'],0,100);
-                            $post_status = $row['post_status'];
-
-
+                       
+                    
+                        $query = "SELECT * FROM posts  LIMIT $page_1, $per_page";
+                        $select_all_posts_query = mysqli_query($connection, $query);
+    
+                        while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                                $post_id= $row['post_id'];
+                                $post_title = $row['post_title'];
+                                $post_author = $row['post_author'];
+                                $post_date = $row['post_date'];
+                                $post_image = $row['post_image'];
+                                $post_content = substr($row['post_content'],0,100);
+                                $post_status = $row['post_status'];
+    
+    
+                            
+                            ?>
                         
-                        ?>
-                    
+    
+                            <!-- First Blog Post -->
+                                    <h2>
+                                        <a href="post.php?p_id=<?php echo $post_id?>"><?php echo $post_title ?></a>
+                                    </h2>
+                                    <p class="lead">
+                                        by <a href="author_posts.php?author=<?php echo $post_author?>&p_id=<?php echo $post_id?>"><?php echo $post_author ?></a>
+                                    </p>
+                                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                                    <hr>
+                                    <a href="post.php?p_id=<?php echo $post_id?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
+                                    <hr>
+                                    <p><?php echo $post_content ?></p>
+                                    <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+    
+                                    <hr>
+                           <?php }
+              
+    
+                            
+                        }else{
+                            $select_post_query_count= "SELECT * FROM posts WHERE post_status = 'published' ";
+                            $find_count = mysqli_query($connection, $select_post_query_count);
+                            $count = mysqli_num_rows($find_count);
+                           echo $count = ceil($count/$per_page);
 
-                        <!-- First Blog Post -->
-                                <h2>
-                                    <a href="post.php?p_id=<?php echo $post_id?>"><?php echo $post_title ?></a>
-                                </h2>
-                                <p class="lead">
-                                    by <a href="author_posts.php?author=<?php echo $post_author?>&p_id=<?php echo $post_id?>"><?php echo $post_author ?></a>
-                                </p>
-                                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
-                                <hr>
-                                <a href="post.php?p_id=<?php echo $post_id?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
-                                <hr>
-                                <p><?php echo $post_content ?></p>
-                                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                             if($count<1){
+                        
+                                echo "<h2> Nothing To See Here Folks</h2>";
+                            }else{
+                                $query = "SELECT * FROM posts  WHERE post_status = 'published' LIMIT $page_1, $per_page";
+                                $select_all_posts_query = mysqli_query($connection, $query);
 
-                                <hr>
+                             while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                                    $post_id= $row['post_id'];
+                                    $post_title = $row['post_title'];
+                                    $post_author = $row['post_author'];
+                                    $post_date = $row['post_date'];
+                                    $post_image = $row['post_image'];
+                                    $post_content = substr($row['post_content'],0,100);
+                                    $post_status = $row['post_status'];
+                                ?>
+                            
 
-                <?php }} ?>
+                                <!-- First Blog Post -->
+                                        <h2>
+                                            <a href="post.php?p_id=<?php echo $post_id?>"><?php echo $post_title ?></a>
+                                        </h2>
+                                        <p class="lead">
+                                            by <a href="author_posts.php?author=<?php echo $post_author?>&p_id=<?php echo $post_id?>"><?php echo $post_author ?></a>
+                                        </p>
+                                        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                                        <hr>
+                                        <a href="post.php?p_id=<?php echo $post_id?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
+                                        <hr>
+                                        <p><?php echo $post_content ?></p>
+                                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                                        <hr>
+
+                        <?php }} }?>
            
          
                 
