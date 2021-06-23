87e97cd722760e8f6891264e3dbd9c1a18413086
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-23 08:37:12 -0400

    fixed no post messaging and navigation bar categories

--- index.php
+++ index.php
@@ -39,45 +39,51 @@
                 $find_count = mysqli_query($connection, $select_post_query_count);
                 $count = mysqli_num_rows($find_count);
 
-                echo $count = ceil($count/$per_page);
+                $count = ceil($count/$per_page);
+
+                if($count<1){
+                   
+                        echo "<h2> Nothing To See Here Folks</h2>";
+                    
+
+                }else{
                 
                    
                 
-                $query = "SELECT * FROM posts WHERE post_status = 'published' LIMIT $page_1, $per_page";
-                $select_all_posts_query = mysqli_query($connection, $query);
+                    $query = "SELECT * FROM posts  WHERE post_status = 'published' LIMIT $page_1, $per_page";
+                    $select_all_posts_query = mysqli_query($connection, $query);
 
-                while($row = mysqli_fetch_assoc($select_all_posts_query)){
-                        $post_id= $row['post_id'];
-                        $post_title = $row['post_title'];
-                        $post_author = $row['post_author'];
-                        $post_date = $row['post_date'];
-                        $post_image = $row['post_image'];
-                        $post_content = substr($row['post_content'],0,100);
-                        $post_status = $row['post_status'];
+                    while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                            $post_id= $row['post_id'];
+                            $post_title = $row['post_title'];
+                            $post_author = $row['post_author'];
+                            $post_date = $row['post_date'];
+                            $post_image = $row['post_image'];
+                            $post_content = substr($row['post_content'],0,100);
+                            $post_status = $row['post_status'];
 
-                        if($post_status == 'published'){
 
-                    }
-                    ?>
-                  
+                        
+                        ?>
+                    
 
-                    <!-- First Blog Post -->
-                            <h2>
-                                <a href="post.php?p_id=<?php echo $post_id?>"><?php echo $post_title ?></a>
-                            </h2>
-                            <p class="lead">
-                                by <a href="author_posts.php?author=<?php echo $post_author?>&p_id=<?php echo $post_id?>"><?php echo $post_author ?></a>
-                            </p>
-                            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
-                            <hr>
-                            <a href="post.php?p_id=<?php echo $post_id?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
-                            <hr>
-                            <p><?php echo $post_content ?></p>
-                            <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                        <!-- First Blog Post -->
+                                <h2>
+                                    <a href="post.php?p_id=<?php echo $post_id?>"><?php echo $post_title ?></a>
+                                </h2>
+                                <p class="lead">
+                                    by <a href="author_posts.php?author=<?php echo $post_author?>&p_id=<?php echo $post_id?>"><?php echo $post_author ?></a>
+                                </p>
+                                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                                <hr>
+                                <a href="post.php?p_id=<?php echo $post_id?>"><img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
+                                <hr>
+                                <p><?php echo $post_content ?></p>
+                                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                            <hr>
+                                <hr>
 
-                <?php }  ?>
+                <?php }} ?>
            
          
                 
