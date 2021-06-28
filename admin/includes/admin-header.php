5b7794128246bbe2112c6112bdc8dab1ad9246d9
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-28 18:59:16 -0400

    fixed style sheet links in headers footers

--- admin/includes/admin-header.php
+++ admin/includes/admin-header.php
@@ -34,16 +34,16 @@ if(!isset($_SESSION['user_role'])){
   
 
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css/sb-admin.css" rel="stylesheet">
+    <link href="/css/sb-admin.css" rel="stylesheet">
    <!-- <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet"> -->
 
-   <link rel="stylesheet" href="css/summernote.css">
+   <link rel="stylesheet" href="/css/summernote.css">
 
     <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- includes/footer.php
+++ includes/footer.php
@@ -12,12 +12,12 @@
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="/js/jquery.js"></script>
      <!-- Bootstrap Core JavaScript -->
-     <script src="js/bootstrap.min.js"></script>
+     <script src="/js/bootstrap.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
 
 </body>
 
