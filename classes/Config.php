dc5bded0b05b7dc1dae65299bbd5a1f085bb6e83
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-06-29 16:26:40 -0400

    added configuration to send mail via mailtrap

---
+++ classes/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+class Config {
+
+    const SMTP_HOST = 'smtp.mailtrap.io';
+    const SMTP_PORT = 2525;
+    const SMTP_USER = "21f384bf2642b4";
+    const SMTP_PASSWORD = "dfa5d9540ef837";
+}
--- forgot.php
+++ forgot.php
@@ -1,5 +1,20 @@
+<?php
+ 
+use PHPMailer\PHPMailer\PHPMailer; // needs to be on top !!!
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require './classes/Config.php'; 
+require './vendor/phpmailer/phpmailer/src/Exception.php';
+require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require './vendor/phpmailer/phpmailer/src/SMTP.php';
+
+?>
 <?php  include "includes/db.php"; ?>
 <?php  include "includes/header.php"; ?>
+<?php 
+
+  
+?>
 
 <?php
     if(!ifItIsMethod('get') && !isset($_GET['forgot'])){
@@ -19,6 +34,52 @@
                 mysqli_stmt_bind_param($stmt, "s", $email);
                 mysqli_stmt_execute($stmt);
                 mysqli_stmt_close($stmt);
+
+
+                /*****
+                 * configure  PHPmailer
+                 * 
+                 * 
+                 * 
+                 * 
+                 * 
+                 */
+                $mail = new PHPMailer();
+                try {
+                    //Server settings
+                    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+                    $mail->isSMTP();                                            //Send using SMTP
+                    $mail->Host       = Config::SMTP_HOST;                     //Set the SMTP server to send through
+                    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                    $mail->Username   = Config::SMTP_USER;                         //SMTP username
+                    $mail->Password   = Config::SMTP_PASSWORD;                             //SMTP password
+                    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+                    $mail->Port       = Config::SMTP_PORT;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                    //$mail->isHTML(true);
+                    //Recipients
+                    $mail->setFrom('joshadamkerbel@gmail.com', 'Dr Josh');
+                    $mail->addAddress($email);     //Add a recipient
+                    // $mail->addAddress('ellen@example.com');               //Name is optional
+                    // $mail->addReplyTo('info@example.com', 'Information');
+                    // $mail->addCC('cc@example.com');
+                    // $mail->addBCC('bcc@example.com');
+                
+                    //Attachments
+                    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+                    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+                
+                    //Content
+                    $mail->isHTML(true);                                  //Set email format to HTML
+                    $mail->Subject = 'Here is the subject';
+                    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                
+                    $mail->send();
+                    echo 'Message has been sent';
+                } catch (Exception $e) {
+                    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                }
+                
             }else{
                 echo "Wrong";
             }
